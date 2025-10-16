5b7dead55a5921598a0f496990c15e837140ae91
Paxius025 <pantongbk@gmail.com>
2025-10-16 13:37:54 +0700

    first version

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './index.css';
+
+export default function App() {
+  return (
+    <>
+      {/* test tailwindcss */}
+      <div className="text-3xl font-bold underline">Hello world!</div>
+    </>
+  );
+}
